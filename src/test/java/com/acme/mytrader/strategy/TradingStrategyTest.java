e5eb5b41baba8e7bd17be39a929366eba5ab19aa
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2022-11-06 23:15:49 +0000

    Update TradingStrategyTest.java

--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -3,7 +3,10 @@ package com.acme.mytrader.strategy;
 import org.junit.Test;
 
 public class TradingStrategyTest {
-    @Test
-    public void testNothing() {
-    }
+   	@Test
+	public void TestTradingStrategy()
+	{
+		assertEquals(0, 0);
+	}
+
 }
